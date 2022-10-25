5d19548ed2acc57aa2b2f47ffec782c6341406fb
Evans Ochieng <evansochieng121@gmail.com>
2022-10-25 14:44:21 +0300

    practice setting up an app environment

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,10 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+
+# require_relative statetments for all necessary files
+require_relative '../lib/garden'
+require_relative '../lib/plant'
+
+#config/environment.rb is also a common place for configuring 
+#application settings e.g. configure access to a database
